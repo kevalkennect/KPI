8cdd37ccb4ac8c9505989a4e8624865de5d98ee5
kevalkennect <keval.panchal@kennect.io>
2022-08-03 18:18:26 +0530

    View Data in Tabel format from CSV Done

---
+++ components/view/table.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-data-table
+    class="elevation-1"
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="10"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: this.$store.getters.getHeaders,
+      desserts: this.$store.getters.getData,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
